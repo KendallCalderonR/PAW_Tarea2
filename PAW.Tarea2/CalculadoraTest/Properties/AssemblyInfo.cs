93058c60072b245f5eb769e531c6a8c534e33cb5
Valeria Monge <Valeria Monge@DESKTOP-KGOH2QL>
2018-10-07 00:37:28 -0600

    Commit Test module, still need some work

---
+++ PAW.Tarea2/CalculadoraTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculadoraTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculadoraTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a37675e-8b14-4b36-90ba-d53f2bd9d043")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PAW.Tarea2/PAW.Tarea2.CalculadoraTest/CalculadorTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+// Aqui es donde necesito ayuda, no puedo invocar .Service.
+using PAW.Tarea2.Service;
+
+
+namespace PAW.Tarea2.CalculadoraTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private IList<string> TestMiListaDeErrores = new List<string>(new string[] { });
+
+        public IList<string> ListaDeErrores
+        {
+            get
+            {
+                return TestMiListaDeErrores;
+            }
+        }
+        [TestMethod]
+
+
+
+        public void primeraRaizAZero() {
+            
+            double testA = 0.0;
+            double testB = 5.0;
+            double testC = 10.0;
+
+            double testExpected = 0.0;
+            double testResult = 0.0;
+            
+            try {
+               var elServicio = new PAW.Tarea2.Service;
+                TestResult = elServicio.PrimeraRaiz(testA, testB, testC);
+
+                Assert.AreEqual(TestResult, testExpected);
+            } catch (Exception ex) {
+                TestMiListaDeErrores.Add("Un expected error primeraRaizAZero.");
+            }
+
+        }
+
+        public void primeraRaizBaseDiscriminanteNegativo()
+        {
+
+            double testA = 5.0;
+            double testB = 5.0;
+            double testC = 3.0;
+
+            double testExpected = 0.0;
+            double testResult = 0.0;
+
+            try
+            {
+                var elServicio = new PAW.Tarea2.Service;
+                TestResult = elServicio.PrimeraRaiz(testA, testB, testC);
+
+                Assert.AreEqual(TestResult, testExpected);
+            }
+            catch (Exception ex)
+            {
+                TestMiListaDeErrores.Add("Un expected error primeraRaizBaseDiscriminanteNegativo.");
+            }
+
+        }
+
+        public void segundaRaizAZero()
+        {
+
+            double testA = 0.0;
+            double testB = 5.0;
+            double testC = 10.0;
+
+            double testExpected = 0.0;
+            double testResult = 0.0;
+
+            try
+            {
+                var elServicio = new PAW.Tarea2.Service;
+                TestResult = elServicio.PrimeraRaiz(testA, testB, testC);
+
+                Assert.AreEqual(TestResult, testExpected);
+            }
+            catch (Exception ex)
+            {
+                TestMiListaDeErrores.Add("Un expected error segundaRaizAZero.");
+            }
+
+        }
+
+        public void segundaRaizBaseDiscriminanteNegativo()
+        {
+
+            double testA = -5.0;
+            double testB = 5.0;
+            double testC = 3.0;
+
+            double testExpected = 0.0;
+            double testResult = 0.0;
+
+            try
+            {
+                var elServicio = new PAW.Tarea2.Service;
+                TestResult = elServicio.PrimeraRaiz(testA, testB, testC);
+
+                Assert.AreEqual(TestResult, testExpected);
+            }
+            catch (Exception ex)
+            {
+                TestMiListaDeErrores.Add("Un expected error segundaRaizBaseDiscriminanteNegativo.");
+            }
+
+        }
+    }
+}
